f06e5062d69ecdb67ba69d4ac1317b9ee4795008
Feisal Hassan <faisalabdi493@gmail.com>
2025-06-11 14:24:46 +0300

    add change

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob'; // must be var to be globally accessible for the test
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // must be var to be globally accessible for the test
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; // must be var to be globally accessible for the test
+}
+
+ const leastFavoriteCustomer = 'bob'; // must be var to be globally accessible for the test
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // will throw an error
+}
